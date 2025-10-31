ba5ff8d9936b3f58376c032f7d1b63d6cbd6a386
Quid-solo <eedo542363@outlook.com>
2025-10-31 18:43:20 +0530

    Prepare backend for deploy on railway

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,11 @@ import cors from "cors"
 
 const app = express()
 
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_ORIGIN || true,
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json({limit: "500kb"}));
 
@@ -19,6 +23,6 @@ import scrapeRouter from './routes/scrape.route.js';
 
 app.use('/api/scrape', scrapeRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
